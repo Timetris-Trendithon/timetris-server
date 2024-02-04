4bc0a0346e0e6e06cbbfe65cb0af4ffc8d3e8b41
jiinkyung <jinkyung010310@gmail.com>
2024-02-04 21:22:55 +0900

    🔧 Chore: 스웨거 세팅 추가

---
+++ src/main/java/com/trendithon/timetris/Config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.trendithon.timetris.Config;// Java
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("TimeTris API 명세서")
+                .description("2024 TRENDITHON TimeTris API 명세서")
+                .version("1.0.0");
+    }
+}
