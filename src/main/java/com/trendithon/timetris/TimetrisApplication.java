a011d31edd82c01c9f0f1336cb2f0ecd8c69e3e1
jiinkyung <jinkyung010310@gmail.com>
2024-02-04 17:52:22 +0900

    🎉 Init: 프로젝트 생성

---
+++ src/main/java/com/trendithon/timetris/TimetrisApplication.java
@@ -0,0 +1,13 @@
+package com.trendithon.timetris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trendithon/timetris/TimetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendithon.timetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
