1f92e2bddacbf2e671dd55abf902e12241043848
jiinkyung <jinkyung010310@gmail.com>
2024-02-17 23:13:57 +0900

    🔧Chore: Swagger에 토큰 추가

--- src/main/java/com/trendithon/timetris/global/config/SwaggerConfig.java
+++ src/main/java/com/trendithon/timetris/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.trendithon.timetris.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +11,26 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+
+        String jwtSchemeName = "JWT TOKEN";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+
         return new OpenAPI()
-                .components(new Components())
+                .components(components)
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
+
+
 
     private Info apiInfo() {
         return new Info()
