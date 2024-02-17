d751273d193df546cfeac5b64978bc55db7f8355
jiinkyung <jinkyung010310@gmail.com>
2024-02-17 23:16:12 +0900

    ✨Feat: 로그인을 위한 User 클래스 생성

--- src/main/java/com/trendithon/timetris/domain/login/domain/User.java
+++ src/main/java/com/trendithon/timetris/domain/login/domain/User.java
@@ -1,20 +1,55 @@
 package com.trendithon.timetris.domain.login.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Entity
 @Getter
-@AllArgsConstructor @NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    private String email;
+
+    @Column
+    private String profile;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String refreshToken;
+
+
+    // 로그인 할 때마다 해당 필드 새롭게 업데이트
+    public User update(String name, String email) {
+        this.name = name;
+        this.email = email;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+    // 유저 권한설정 메소드
+    public void authorizeUser() {
+        this.role = Role.USER;
+    }
+
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
 
 }
