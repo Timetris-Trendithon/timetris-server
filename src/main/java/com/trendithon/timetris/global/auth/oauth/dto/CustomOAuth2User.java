b813bb886583ac3cff021fe2236168f5ba66da5c
jiinkyung <jinkyung010310@gmail.com>
2024-02-18 20:46:26 +0900

    🐛Fix: 이메일이 잘못 받아와지는 오류 해결

---
+++ src/main/java/com/trendithon/timetris/global/auth/oauth/dto/CustomOAuth2User.java
@@ -0,0 +1,25 @@
+package com.trendithon.timetris.global.auth.oauth.dto;
+
+import com.trendithon.timetris.domain.login.domain.Role;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@Setter
+public class CustomOAuth2User extends DefaultOAuth2User {
+    private String email;
+    private Role role;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes, String nameAttributeKey,
+                            String email, Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.role = role;
+    }
+}
