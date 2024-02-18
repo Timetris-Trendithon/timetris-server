59d5e0067deceae429041f43d76af3bc597f39a4
jiinkyung <jinkyung010310@gmail.com>
2024-02-18 11:58:20 +0900

    🔨Chore: 배포 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,7 +19,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 
 nohup sudo java -jar \
-        -Dspring.config.location=/home/ubuntu/app/application.yml, /home/ubuntu/app/application-jwt.yml, /home/ubuntu/app/application-oauth.yml \
+        -Dspring.config.location=/home/ubuntu/app/application.yml \
         -Duser.timezone=Asia/Seoul\
         $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
