760cc3f35780391f06c274dc1e60e066a34d2af6
jiinkyung <jinkyung010310@gmail.com>
2024-02-21 10:36:59 +0900

    ✨Feat: 회원 탈퇴 API

--- src/main/java/com/trendithon/timetris/global/exception/enums/SuccessStatus.java
+++ src/main/java/com/trendithon/timetris/global/exception/enums/SuccessStatus.java
@@ -12,7 +12,8 @@ public enum SuccessStatus {
     /**
      * 200 OK
      */
-    OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+    OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+    DELETE_USER_SUCCESS(HttpStatus.OK, "USER200", "유저 탈퇴 성공입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
